bd10a17738036f18995f27bc2f9a3ab2b3795931
Marco <marcocayetanojr@gmail.com>
2020-03-09 15:37:03 -0700

    react-1-afternoon-done

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 
 
-class EvenAndOdd extends Component {
+export default class EvenAndOdd extends Component {
+constructor() {
+    super();
+
+    this.state = {
+        evenArray: [],
+        oddArray: [],
+        userInput: ''
+    }
+}
+
+handleChange(val) {
+    this.setState({ userInput: val });
+}
+
+assignEvenAndOdds(userInput) {
+    let arr = userInput.split(',');
+    let evens = [];
+    let odds = [];
+
+    for ( let i = 0; i < arr.length; i++ ) {
+      if ( arr[i] % 2 === 0 ) {
+        evens.push( arr[i] );
+      } else {
+        odds.push( arr[i] );
+      }
+    }
+
+    this.setState({ 
+      evenArray: evens, 
+      oddArray: odds 
+    });
+  }
+
     render() {
+      console.log(this.state.evenArray)
+      console.log(this.state.oddArray)
         return(
-            <p> EvenAndOdd </p>
+            <div className="puzzleBox evenAndOddPB">
+            <h4>Evens and Odds</h4>
+            <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }/>
+            <button className="confirmationButton" onClick={ () => { this.assignEvenAndOdds(this.state.userInput) }}> Split </button>
+            <span className="resultsBox">Evens: {this.state.evenArray} </span>
+            <span className="resultsBox">Odds: {this.state.oddArray} </span>
+            </div>
+            
         )
     }
 }
 
 
 
-export default EvenAndOdd;
+
--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 
 
-class FilterString extends Component {
-    render() {
+
+
+
+
+export default class FilterString extends Component {
+
+    constructor (Props){
+        super(Props);
+        this.state = {
+            names:  ['Marco', 'Peter', 'Patrick', 'Justin', 'Jacob', 'Mezz'],
+            userInput:  '',
+            filteredNames: []
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+
+    filterNames(userInput) {
+        let names = this.state.names;
+        let filteredNames = [];
+    
+        for ( let i = 0; i < names.length; i++ ) {
+          if ( names[i].includes(userInput) ) {
+            filteredNames.push(names[i]);
+          }
+        }
+    
+        this.setState({ filteredNames: filteredNames });
+      }
+    
+
+
+render() {
         return(
-            <p> FilterString </p>
+            <div className="puzzleBox filterStringPB">
+                <h4>Filter String</h4>
+                <span className="puzzleText">Names: { JSON.stringify(this.state.names, null, 10) }</span>
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }/>
+                <button className="confirmationButton" onClick={ () => this.filterNames(this.state.userInput) }>Filter</button>
+                <span className="resultsBox filterStringRB">Filtered Names: { JSON.stringify(this.state.filteredNames, null, 10) }</span>
+            </div>
         )
     }
 }
 
 
-
-export default FilterString;
